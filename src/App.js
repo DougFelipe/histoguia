68eb20b0b4883677bb5e5dd4d7b4907544995bd4
eloisafernandes <eloisafer16@gmail.com>
2024-04-10 17:20:42 -0300

    projeto inicial HistoGuia

--- src/App.js
+++ src/App.js
@@ -3,21 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-screen	bg-jet flex justify-center items-center">
+      <h1 className="text-3xl text-cinereous font-bold underline">
+        HistoGuia
+      </h1>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'jet': '#292929',
+        'cinereous': '#A58E8D',
+      },
+    },
+  },
+  plugins: [],
+}
+
