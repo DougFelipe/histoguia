76a110d75a83b8d4a7068acc11d771797f1ac7d5
Willian Costa <68620087+WillianCosta12@users.noreply.github.com>
2024-06-12 21:37:57 -0300

    mobileAdmAluno

--- src/components/Adm/SearchBarAluno.js
+++ src/components/Adm/SearchBarAluno.js
@@ -28,18 +28,23 @@ const SearchBarAluno = () => {
     }
   };
 
-  const handleChecked = (event) => {
+  const handleChecked = () => {
     if (check === false) {
       setColor("#F9FAFC");
     } else {
       setColor("#ffffff");
     }
-    setCheck(event.target.checked);
+
+    setCheck(!check);
+  };
+
+  const deleteAluno = (pos) => {
+    console.log(pos);
   };
 
   return (
-    <div className="flex flex-col items-center justify-center w-11/12 p-4">
-      <div className="w-full">
+    <div className="flex flex-col items-center justify-center w-11/12 p-4 max-md:p-0 max-md:w-[250px]">
+      <div className="w-full max-md:w-[250px]">
         <div className="relative">
           <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
             <svg
@@ -63,12 +68,12 @@ const SearchBarAluno = () => {
             placeholder="Search"
             value={query}
             onChange={handleInputChange}
-            className="w-full p-2 pl-10 border rounded-[50px] focus:outline-none focus:ring-2 focus:ring-gray-300 text-[14px] font-inter-regular"
+            className="w-full p-2 pl-10 border rounded-[50px] focus:outline-none focus:ring-2 focus:ring-gray-300 text-[14px] font-inter-regular max-md:w-[250px]"
           />
         </div>
         {results.length > 0 && (
-          <div className="mt-6 gap-4 divide-y divide-gray">
-            <div className="bg-[#FFFFFF] flex flex-row items-center px-[20px] py-[15px] font-semibold text-[20px] rounded-t-lg">
+          <div className="mt-6 gap-4 divide-y divide-gray max-md:mt-2 max-md:mb-6">
+            <div className="bg-[#FFFFFF] flex flex-row items-center px-[20px] py-[15px] font-semibold text-[20px] rounded-t-lg max-md:hidden">
               <label
                 class="relative flex items-center mr-[30px] rounded-full cursor-pointer"
                 htmlFor="check"
@@ -105,71 +110,87 @@ const SearchBarAluno = () => {
             {results.map((result, index) => (
               <div
                 key={index}
-                className={`px-[20px] py-[10px] bg-[${color}] flex flex-row items-center`}
+                className={`px-[20px] py-[10px] bg-[${color}] flex flex-row items-center max-md:flex-col max-md:gap-4 max-md:w-[250px] max-md:mb-[10px] max-md:rounded-lg max-md:items-start`}
                 id={index}
               >
-                <label
-                  class="relative flex items-center mr-[30px] rounded-full cursor-pointer"
-                  htmlFor="check"
-                >
-                  <input
-                    type="checkbox"
-                    class="before:content[''] peer relative h-[40px] w-[40px] cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
-                    id="check"
-                    checked={check}
-                    onChange={handleChecked}
-                  />
-                  <span class="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      class="h-5 w-5"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                      stroke="currentColor"
-                      stroke-width="1"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                        clip-rule="evenodd"
-                      ></path>
-                    </svg>
-                  </span>
-                </label>
-                <div className="w-[320px] flex flex-row">
-                  <img
-                    className="w-[40px] h-[40px] rounded-[12px]"
-                    src={result.img}
-                    alt={result.nome}
-                  />
-                  <div className="flex flex-col justify-center ml-[10px]">
-                    <span className="text-left text-[14px] font-primary font-inter-semi">
-                      {result.nome}
+                <div className="flex max-md:flex-row-reverse max-md:gap-4">
+                  <label
+                    class="relative flex items-center mr-[30px] rounded-full cursor-pointer max-md:mr-[0px]"
+                    htmlFor="check"
+                  >
+                    <input
+                      type="checkbox"
+                      class="before:content[''] peer relative h-[40px] w-[40px] appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
+                      id={index}
+                      checked={check}
+                      disabled
+                    />
+                    <span class="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-5 w-5"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        stroke="currentColor"
+                        stroke-width="1"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
                     </span>
-                    <span className="text-left text-[14px] text-[#959595] font-inter">
-                      {result.username}
-                    </span>
+                  </label>
+                  <div className="w-[320px] flex flex-row">
+                    <img
+                      className="w-[40px] h-[40px] rounded-[12px]"
+                      src={result.img}
+                      alt={result.nome}
+                    />
+                    <div className="flex flex-col justify-center ml-[10px]">
+                      <span className="text-left text-[14px] font-primary font-inter-semi max-md:text-[12px]">
+                        {result.nome}
+                      </span>
+                      <span className="text-left text-[14px] text-[#959595] font-inter">
+                        {result.username}
+                      </span>
+                    </div>
                   </div>
                 </div>
-                <div className="w-[150px]">
+                <div className="w-[150px] flex gap-[80px] max-md:flex-row">
+                  <span className="hidden text-[#959595] text-[15px] p-0 max-md:block">
+                    Status
+                  </span>
                   <Status status={result.status} />
                 </div>
 
-                <div className="w-[330px]">
+                <div className="w-[330px] items-center flex gap-[10px] max-md:flex-row">
+                  <span className="hidden text-[#959595] text-[15px] p-0 max-md:block">
+                    Email
+                  </span>
                   <span className="block text-left text-[12px] font-[#9098A3] font-inter-regular">
                     {result.email}
                   </span>
                 </div>
-                <div className="w-[100px]">
+                <div className="w-[100px] flex gap-[80px] max-md:flex-row ">
+                  <span className="hidden text-[#959595] text-[15px] p-0 max-md:block">
+                    Birthday
+                  </span>
                   <span className="block text-left text-[12px] font-[#9098A3] font-inter-regular">
                     {result.birthday}
                   </span>
                 </div>
-                <div className="w-[80px]">
-                  <i className="fa-solid fa-circle-info fa-xl"></i>
-                </div>
-                <div className="w-[50px]">
-                  <i className="fa-regular fa-trash-can fa-xl"></i>
+                <div className="flex max-md:flex-row max-md:justify-end">
+                  <div className="w-[80px] max-md:w-[40px]">
+                    <i className="fa-solid fa-circle-info fa-xl"></i>
+                  </div>
+                  <div className="w-[50px] max-md:w-[20px] cursor-pointer">
+                    <i
+                      className="fa-regular fa-trash-can fa-xl"
+                      onClick={() => deleteAluno(index)}
+                    ></i>
+                  </div>
                 </div>
               </div>
             ))}
--- src/pages/AdmAlunos.js
+++ src/pages/AdmAlunos.js
@@ -17,15 +17,15 @@ const AdmAlunos = () => {
           isSidebarOpen ? "md:ml-64" : "ml-0"
         }`}
       >
-        <div className="flex flex-row justify-between items-center mt-12 pl-[100px] p-4">
-          <p className="text-[24px] font-semibold">Alunos</p>
-          <button className="w-[140px] h-[48px] bg-[#7839CD] text-[#FFFFFF] rounded-lg">
+        <div className="flex flex-row justify-between items-center mt-12 pl-[100px] p-4 max-md:flex-col max-md:gap-6 max-md:p-0 max-md: max-md:w-[200px] max-md:pl-[120px]">
+          <p className="text-[24px] font-semibold max-md:text-[20px]">Alunos</p>
+          <button className="w-[140px] h-[48px] bg-[#7839CD] text-[#FFFFFF] rounded-lg max-md:w-[250px]">
             <span className="mr-[10px]">Download</span>
             <i class="fa fa-cloud-download" aria-hidden="true"></i>
           </button>
         </div>
 
-        <div className="flex flex-row justify-end items-center mt-4">
+        <div className="flex flex-row justify-end items-center mt-4 max-md:w-[320px] max-md:justify-center">
           <SearchBarAluno />
         </div>
       </div>
