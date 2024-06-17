865e7e81724a05bf68058f180907c3712735f9c0
Willian Costa <68620087+WillianCosta12@users.noreply.github.com>
2024-06-17 20:43:12 -0300

    routeLogin

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,16 +1,33 @@
 import Icon from "./icon";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
 
 function Login() {
+  const [user, setUser] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    if (user === "admin") {
+      navigate("/admAlunos");
+    } else {
+      navigate("/home");
+    }
+  };
+  const handleChange = (event) => {
+    setUser(event.target.value);
+  };
+
   return (
     <div className="bg-[#F1F0F3] w-[740px] h-[573px] flex flex-col items-center max-md:h-full ">
       <h1 className="mx-[225px] mt-[75px] mb-[20px] text-[60px] text-[#3D2E7C] font-medium max-md:mx-[30px] max-md:text-[45px] max-md:mb-[10px]">
         Histo <span className="text-[#8D8D8D] font-bold">Guia</span>
       </h1>
-      <form className="flex flex-col">
+      <form className="flex flex-col" onSubmit={handleSubmit}>
         <input
-          type="email"
+          type="text"
           className="w-[328px] h-[46px] mx-[202.5px] mb-[20px] px-[20px] bg-[#F1F0F3] text-[#6C6A6A] font-regular border-solid border-2 border-[#6C6A6A]/[.6] hover:border-[#6C6A6A] focus:border-[#6C6A6A] outline-none rounded-lg placeholder:text-[#6C6A6A] placeholder:font-regular max-md:mx-[25px] max-md:w-[250px]"
+          value={user}
+          onChange={handleChange}
           placeholder="Enter email"
         />
         <input
@@ -25,18 +42,17 @@ function Login() {
         >
           Forget Password?
         </a>
-        <Link
+        {/*<Link
           to="/home"
           className="w-[328px] h-[46px] mx-[202.5px] mt-[20px] bg-[#3D2E7C] text-[#FFFFFF] text-[18px] font-regular rounded-lg flex items-center justify-center max-md:mx-[25px] max-md:w-[250px]"
         >
           Login
-        </Link>
-        {/* <input
-        href = "/quiz"
+        </Link>*/}
+        <input
           type="submit"
           value="Login"
           className="w-[328px] h-[46px] mx-[202.5px] mt-[20px] bg-[#3D2E7C] text-[#FFFFFF] text-[18px] font-regular rounded-lg"
-        /> */}
+        />
       </form>
       <p className="text-[#757171] mx-[315px] mt-[15px] mb-[20px] max-md:mx-[104px] max-md:text-[14px]">
         Or sign up with
